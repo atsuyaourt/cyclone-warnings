68796e551e4a408c7aaa13e237485dc1beaabc71
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-07-31 00:30:54 +0800

    Major overhaul on jtwc library

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 var jtwc = require('./lib/jtwc');
 
 //jtwc.parse('wp1117web.txt');
-jtwc.fetch().then(ret => console.log(ret));
+//jtwc.fetch().then(ret => console.log(ret));
+
+// jtwc.list().then(ret => console.log(ret));
+
+//jtwc.get('10E').then(ret => ret.warn_info.getInfo().then(a => console.log(a)));
