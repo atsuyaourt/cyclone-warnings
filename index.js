00c215b767f539a130720685f29ce7994dd27a9e
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-08-01 20:24:06 +0800

    added unit testing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
-var jtwc = require('./lib/jtwc');
+const jtwc = require('./lib/jtwc');
 
 //jtwc.parse('wp1117web.txt');
 //jtwc.fetch().then(ret => console.log(ret));
 
-// jtwc.list().then(ret => console.log(ret));
+jtwc.list().then(ret => console.log(ret));
 
 //jtwc.get('10E').then(ret => ret.warn_info.getInfo().then(a => console.log(a)));
